2e017a5e05f79de28d512bd04ed12ad039fce889
I Putu Raditya Dharma Yoga <iprdy.real@gmail.com>
2025-06-09 16:26:53 +0800

    Menambahkan method deleteVillaRoomTypes di VillasController

--- src/controllers/VillasController.java
+++ src/controllers/VillasController.java
@@ -147,6 +147,20 @@ public class VillasController {
         }
     }
 
+    //DELETE /villas/rooms/{id}
+    public void deleteVillaRoomTypes(int id) throws SQLException {
+        //Get villa untuk mengecek apakah villanya ada
+
+        String sql = "DELETE from room_types WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:villa_booking.db");
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            System.out.println("Has connected to the database");
+
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        }
+    }
+
     //DELETE /villas/{id} => Menghapus data suatu villa
     public void deleteVilla(int id) throws SQLException {
         //Get villa untuk mengecek apakah villa nya ada
