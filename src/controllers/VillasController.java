dfd79d36ba8e960c0a44ea06bba556d9781521bd
I Putu Raditya Dharma Yoga <iprdy.real@gmail.com>
2025-06-09 14:17:58 +0800

    Menambahkan method createVillasRooms di VillasController

--- src/controllers/VillasController.java
+++ src/controllers/VillasController.java
@@ -1,19 +1,21 @@
 package controllers;
 
+import models.RoomTypes;
 import models.Villas;
 
 import java.sql.*;
 
 public class VillasController {
 
-    //POST /villas /menambahkan data vila
+    //POST /villas => menambahkan data vila
     public Villas createVilla(Villas villa) throws SQLException {
         String sql = "INSERT INTO villas (id, name, description, address) VALUES (?, ?, ?, ?)";
 
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:villa_booking.db");
-        PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+        PreparedStatement ps = conn.prepareStatement(sql)) {
             System.out.println("Has connected to the database");
 
+            ps.setInt(1, villa.getId());
             ps.setString(1, villa.getName());
             ps.setString(2, villa.getDescription());
             ps.setString(3, villa.getAddress());
@@ -23,15 +25,46 @@ public class VillasController {
                 throw new SQLException("Failed to create villa");
             }
 
-            try (ResultSet rs = ps.getGeneratedKeys()) {
-                if (rs.next()) {
-                    villa.setId(rs.getInt(1));
-                } else {
-                    throw new SQLException("Failed to create villa, no ID obtained");
-                }
+            return villa;
+
+        } catch (SQLException e) {
+            System.out.println("Database error: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    //POST /villas/{id}/rooms => Menambahkan tipe kamar pada vila
+    public RoomTypes createVillasRooms(RoomTypes roomtypes) throws SQLException {
+        String sql = """
+                INSERT INTO villasrooms (id, villa, name, quantity, capacity, price, bed_size, has_desk, has_ac, has_tv,has_wifi,has_shower,has_hotwater,has_fridge) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """;
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:villa_booking.db");
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+            System.out.println("Has connected to the database");
+
+            ps.setInt(1, roomtypes.getId());
+            ps.setInt(2, roomtypes.getVilla_id());
+            ps.setString(3, roomtypes.getName());
+            ps.setInt(4, roomtypes.getQuantity());
+            ps.setInt(5, roomtypes.getCapacity());
+            ps.setInt(6, roomtypes.getPrice());
+            ps.setString(7, roomtypes.getBed_size());
+            ps.setBoolean(8, roomtypes.isHas_desk());
+            ps.setBoolean(9, roomtypes.isHas_ac());
+            ps.setBoolean(10, roomtypes.isHas_tv());
+            ps.setBoolean(11, roomtypes.isHas_wifi());
+            ps.setBoolean(12, roomtypes.isHas_shower());
+            ps.setBoolean(13, roomtypes.isHas_hotwater());
+            ps.setBoolean(14, roomtypes.isHas_fridge());
+
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Failed to create room type villa");
             }
 
-            return villa;
+            return roomtypes;
 
         } catch (SQLException e) {
             System.out.println("Database error: " + e.getMessage());
