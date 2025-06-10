aeea7ed86e7fff8f620f0bbb02bb5c8e8a02bc24
Elika <rey048286@gmail.com>
2025-06-10 16:36:41 +0800

    Membuat Class Customer

---
+++ src/models/Customer.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Customer(int id, String name, String email, String phone){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
