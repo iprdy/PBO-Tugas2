e110ec702e2ac6a35a93b92c38d734e7251bc961
gerald <tgeraldhizkia@gmail.com>
2025-06-15 19:22:42 +0800

    biar bisa lihat daftar booking yang telah dilakukan oleh seorang customer

--- src/controllers/CustomerController.java
+++ src/controllers/CustomerController.java
@@ -5,7 +5,7 @@ import api.Response;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import models.Customer;
-
+import models.Booking;
 import java.sql.*;
 import java.util.*;
 
@@ -15,6 +15,52 @@ public class CustomerController {
 
     public CustomerController(Connection conn) {
         this.conn = conn;
+    }
+
+    //GET /customer/{id}/bookings => daftar booking yang telah dilakukan oleh seorang customer
+    public void getCustomerBookings(HttpExchange httpExchange) {
+        Request req = new Request(httpExchange);
+        Response res = new Response(httpExchange);
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:../villa_booking.db")) {
+            // Ambil ID dari path
+            String[] pathParts = httpExchange.getRequestURI().getPath().split("/");
+            int customerId = Integer.parseInt(pathParts[2]);
+
+            String sql = "SELECT * FROM bookings WHERE customer = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, customerId);
+            ResultSet rs = ps.executeQuery();
+
+            List<Booking> bookings = new ArrayList<>();
+            while (rs.next()) {
+                Integer voucher = rs.getObject("voucher") != null ? rs.getInt("voucher") : null;
+
+                Booking booking = new Booking(
+                        rs.getInt("customer"),
+                        rs.getInt("room_type"),
+                        rs.getString("checkin_date"),
+                        rs.getString("checkout_date"),
+                        rs.getInt("price"),
+                        voucher,
+                        rs.getInt("final_price")
+                );
+
+                booking.setId(rs.getInt("id"));
+                booking.setPaymentStatus(rs.getString("payment_status"));
+                booking.setHasCheckedin(rs.getBoolean("has_checkedin"));
+                booking.setHasCheckedout(rs.getBoolean("has_checkedout"));
+                bookings.add(booking);
+            }
+
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(bookings);
+            res.json(json);
+
+        } catch (Exception e) {
+            res.error("Failed to fetch bookings: " + e.getMessage());
+        }
     }
 
     public void getAllCustomers(HttpExchange httpExchange) {
