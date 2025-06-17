0ca262bc5bb0817c765d5625e6ca662e13b9bd8a
Elika <rey048286@gmail.com>
2025-06-17 16:34:42 +0800

    Membuat Class Voucher

---
+++ src/models/Voucher.java
@@ -0,0 +1,20 @@
+package models;
+
+public class Voucher {
+    private int id;
+    private String code;
+    private String description;
+    private double discount;
+    private String startDate;
+    private String endDate;
+
+    // Constructor lengkap
+    public Voucher(int id, String code, String description, double discount, String startDate, String endDate) {
+        this.id = id;
+        this.code = code;
+        this.description = description;
+        this.discount = discount;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
