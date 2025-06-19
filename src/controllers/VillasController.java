8543ec6859ba37d8fc8d74a76247d66e5996cfde
Elika <rey048286@gmail.com>
2025-06-19 15:39:47 +0800

    Menghapus field Id pada query karena sudah autoincrement

--- src/controllers/VillasController.java
+++ src/controllers/VillasController.java
@@ -201,9 +201,9 @@ public class VillasController {
     // POST /villas/{id}/rooms => Menambahkan tipe kamar pada vila
     public RoomTypes createVillasRooms(RoomTypes roomtypes) throws SQLException {
         String sql = """
-                INSERT INTO room_types (id, villa, name, quantity, capacity, price, bed_size, has_desk, has_ac, has_tv,
+                INSERT INTO room_types (villa, name, quantity, capacity, price, bed_size, has_desk, has_ac, has_tv,
                 has_wifi, has_shower, has_hotwater, has_fridge)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                 """;
 
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:villa_booking.db");
@@ -211,20 +211,19 @@ public class VillasController {
 
             System.out.println("Connected to the database");
 
-            ps.setInt(1, roomtypes.getId());
-            ps.setInt(2, roomtypes.getVilla_id());
-            ps.setString(3, roomtypes.getName());
-            ps.setInt(4, roomtypes.getQuantity());
-            ps.setInt(5, roomtypes.getCapacity());
-            ps.setInt(6, roomtypes.getPrice());
-            ps.setString(7, roomtypes.getBed_size());
-            ps.setBoolean(8, roomtypes.isHas_desk());
-            ps.setBoolean(9, roomtypes.isHas_ac());
-            ps.setBoolean(10, roomtypes.isHas_tv());
-            ps.setBoolean(11, roomtypes.isHas_wifi());
-            ps.setBoolean(12, roomtypes.isHas_shower());
-            ps.setBoolean(13, roomtypes.isHas_hotwater());
-            ps.setBoolean(14, roomtypes.isHas_fridge());
+            ps.setInt(1, roomtypes.getVilla_id());
+            ps.setString(2, roomtypes.getName());
+            ps.setInt(3, roomtypes.getQuantity());
+            ps.setInt(4, roomtypes.getCapacity());
+            ps.setInt(5, roomtypes.getPrice());
+            ps.setString(6, roomtypes.getBed_size());
+            ps.setBoolean(7, roomtypes.isHas_desk());
+            ps.setBoolean(8, roomtypes.isHas_ac());
+            ps.setBoolean(9, roomtypes.isHas_tv());
+            ps.setBoolean(10, roomtypes.isHas_wifi());
+            ps.setBoolean(11, roomtypes.isHas_shower());
+            ps.setBoolean(12, roomtypes.isHas_hotwater());
+            ps.setBoolean(13, roomtypes.isHas_fridge());
 
             int affectedRows = ps.executeUpdate();
             if (affectedRows == 0) {
